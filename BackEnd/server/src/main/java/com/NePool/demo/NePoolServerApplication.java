0850ddf9357992abe2836e065f80b4d2b9195953
jinyunjae <jinyun3075@naver.com>
2022-02-23 15:33:45 +0900

    Init Server

---
+++ BackEnd/server/src/main/java/com/NePool/demo/NePoolServerApplication.java
@@ -0,0 +1,13 @@
+package com.NePool.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NePoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NePoolServerApplication.class, args);
+	}
+
+}
---
+++ BackEnd/server/src/test/java/com/NePool/demo/NePoolServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NePool.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NePoolServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
