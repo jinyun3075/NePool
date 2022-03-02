0d6751051baeaaf1daaf95f787b7b520962c08bd
jinyunjae <jinyun3075@naver.com>
2022-03-02 23:23:48 +0900

    Add ErrorHandle

--- BackEnd/server/src/main/java/com/NePool/app/service/UserServiceImpl.java
+++ BackEnd/server/src/main/java/com/NePool/app/service/UserServiceImpl.java
@@ -3,9 +3,10 @@ package com.NePool.app.service;
 import com.NePool.app.dto.PageRequestDTO;
 import com.NePool.app.dto.PageResultDTO;
 import com.NePool.app.dto.UserDTO;
-import com.NePool.app.entity.User;
+import com.NePool.app.entity.NePoolUser;
 import com.NePool.app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +16,17 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService{
     public final UserRepository repository;
     @Override
-    public UserDTO register(UserDTO dto) {
-        User res = repository.save(dtoToEntity(dto));
+    public UserDTO register(UserDTO dto) throws Exception {
+        if(dto.getName().equals("")){
+            throw new Exception("이름을 입력해주세요.");
+        }
+        NePoolUser res = repository.save(dtoToEntity(dto));
         return entityToDto(res);
+        
     }
 
     @Override
-    public PageResultDTO<UserDTO, User> getList(PageRequestDTO dto) {
+    public PageResultDTO<UserDTO, NePoolUser> getList(PageRequestDTO dto) {
         return null;
     }
 }
