9eed5e045101eef0523ef355eb9d4564795f7398
sichan <sichan1301@naver.com>
2022-03-03 15:35:55 +0900

    Add Mypage Markup & Router Library

--- FrontEnd/workbook/src/App.js
+++ FrontEnd/workbook/src/App.js
@@ -1,13 +1,18 @@
 import Main from './pages/Main';
 import Login from './pages/Login';
+import Mypage from './pages/Mypage';
 import "./style/globals.css";
-
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Main />
-      <Login /> 
+      <Route path="/main">
+        <Main />
+      </Route>
+        <Login /> 
+        <Mypage />
+
     </>
     )
 }
