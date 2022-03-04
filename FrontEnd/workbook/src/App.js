d7c95c135191affe8d13def83f69e5967744465a
choisung2 <c0817d@gmail.com>
2022-03-04 15:12:54 +0900

    Update Route

--- FrontEnd/workbook/src/App.js
+++ FrontEnd/workbook/src/App.js
@@ -2,17 +2,18 @@ import Main from './pages/Main';
 import Login from './pages/Login';
 import Mypage from './pages/Mypage';
 import "./style/globals.css";
-import {Route} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
+import Join from './pages/Join';
 
 function App() {
   return (
     <>
-      <Route path="/main">
-        <Main />
-      </Route>
-        <Login /> 
-        <Mypage />
-
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
     </>
     )
 }
