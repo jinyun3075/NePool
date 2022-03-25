d50ecd86cad882c0e59c63ac28a2169b1c6260b9
choisung2 <c0817d@gmail.com>
2022-03-25 15:01:14 +0900

    Update StudyPage

--- FrontEnd/workbook/src/components/study/Progress.js
+++ FrontEnd/workbook/src/components/study/Progress.js
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
+import { COLORS } from '../../constants';
 
 export default function Progress({ total, currentQuestion }) {
 
   return (
     <>
       <ProgressCheck>
-        Qusetion {currentQuestion + 1} of {total}
+       문제 {currentQuestion + 1} of {total}
       </ProgressCheck>
     </>
   )
 }
 
 const ProgressCheck = styled.h2`
-  font-size: 16px;
-  margin: 5px 20px;
+  font-size: 15px;
+  margin: 6px 20px;
+  color: ${COLORS.gray}
 `
