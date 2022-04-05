a52f6f755933f73eabaa0eadb52825e924216a55
Mangopapa1 <gustp26@gmail.com>
2022-04-06 04:53:50 +0900

    Add 페이징 기능 추가

--- FrontEnd/workbook/src/components/post/postButton.js
+++ FrontEnd/workbook/src/components/post/postButton.js
@@ -2,27 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import { COLORS } from "../../constants";
 
-export default function PostBtn({ total, limit, page, setPage }) {
-  const numPages = Math.ceil(total / limit);
+export default function PostBtn({page, onClickNum}) {
 
   return (
     <>
       <Nav>
-        <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        <Button >
           &lt;
         </Button>
-        {Array(numPages)
-          .fill()
-          .map((_, i) => (
-            <Button
-              key={i + 1}
-              onClick={() => setPage(i + 1)}
-              aria-current={page === i + 1 ? "page" : null}
-            >
-              {i + 1}
-            </Button>
-          ))}
-        <Button onClick={() => setPage(page + 1)} disabled={page === numPages}>
+        {page.map((number)=>{
+          return (
+        <Button key={number} value={number} onClick={onClickNum}>
+          {number}
+        </Button>
+          )
+        })}
+        <Button>
           &gt;
         </Button>
       </Nav>
@@ -45,7 +40,7 @@ const Nav = styled.nav`
   height: 30px;
   /* padding: 8px; */
   margin: 0;
-  background: ${COLORS.black};
+  background: ${COLORS.blue};
   color: white;
   font-size: 15px;
 
