b0c8d9e42af1141ecf8acb031c5fbc45eb3fc8b1
choisung2 <c0817d@gmail.com>
2022-04-19 22:10:24 +0900

    Refector Notice Component

--- FrontEnd/workbook/src/components/notice/NoticePost.js
+++ FrontEnd/workbook/src/components/notice/NoticePost.js
@@ -1,16 +1,15 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
 import { API, COLORS } from '../../constants'
 
 export default function NoticePost() {
-
   const navigate = useNavigate();
 
-  const location = useLocation()
-  const item = location.state.item
-  const userId = location.state.userId
+  const location = useLocation();
+  const item = location.state.item;
+  const userId = location.state.userId;
 
   const [notice, setNotice] = useState({
     contents: "",
@@ -18,143 +17,118 @@ export default function NoticePost() {
     modDate: "",
     regDate: "",
     title: "",
-  })
+  });
 
   const getNotice = async() => {
     const res = await axios.get(`${API}/announcement/show/${item.id}` , {
       headers: {
         'Content-Type': 'application/json',
       },
-    })
+    });
     setNotice(res.data);
-  }
+  };
 
   useEffect(() => {
-    getNotice()
-  }, [])
+    getNotice();
+  }, []);
 
   const deleteNotice = async (e) => {
-    e.preventDefault()
-    try {
-      await axios.delete(`${API}/announcement/${userId}`, {
-        data: notice.id,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-    })
-      navigate('/notice')
-    } catch(err) {
-      console.log(err);
-    }
-  }
+    e.preventDefault();
+    await axios.delete(`${API}/announcement/${userId}`, {
+      data: notice.id,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    navigate('/notice');
+  };
 
   return (
-    <>
-      <NoticeForm>
-        <NoticeTitle>{item.title}</NoticeTitle>
-        <InfoBox>
-          <Info>작성자: NEPOOLADMIN</Info>
-          <InfoData>
-            {/* <Info>수정 일시 : {item.modDate.slice(0, 10)}</Info> */}
-            <Info>작성 일시 : {item.regDate.slice(0, 10)}</Info>
-          </InfoData>
-        </InfoBox>
-        
-        <Line></Line>
-        <InputBox>
-          <Desc dangerouslySetInnerHTML={{ __html: item.contents }} />
-        </InputBox>
-        {userId === "NEPOOLADMIN" && (
+    <NoticeForm>
+      <NoticeTitle>{item.title}</NoticeTitle>
+      <InfoBox>
+        <Info>작성자: NEPOOLADMIN</Info>
+        <InfoData>
+          {/* <Info>수정 일시 : {item.modDate.slice(0, 10)}</Info> */}
+          <Info>작성 일시 : {item.regDate.slice(0, 10)}</Info>
+        </InfoData>
+      </InfoBox>
+      <Line></Line>
+      <InputBox>
+        <Desc dangerouslySetInnerHTML={{ __html: item.contents }} />
+      </InputBox>
+      {userId === "NEPOOLADMIN" && (
         <BtnBox>
           <Link to={`/notice/editor/${notice.id}`} state={{notice: notice}} >
             <Btn type="submit">수정</Btn>
           </Link>
           <Btn onClick={deleteNotice}>삭제</Btn>
         </BtnBox>
-        )}
-      </NoticeForm>
-    </>
+      )}
+    </NoticeForm>
   );
 }
 
 const NoticeForm = styled.form`
-  width: 70%;
   margin: 70px auto;
+  width: 70%;
 `;
 
 const NoticeTitle = styled.h2`
   font-size: 24px;
-`
+`;
 
 const InfoBox = styled.div`
-margin: 15px 0;
   display: flex;
-  /* flex-direction: column; */
   justify-content: space-between;
   gap: 5px;
-`
+  margin: 15px 0;
+`;
 
-const InfoData = styled.div``
+const InfoData = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Info = styled.p`
-  font-size: 13px;
   color: ${COLORS.light_gray};
-`
+  font-size: 13px;
+`;
 
 const Line = styled.div`
-  border: 0.5px solid ${COLORS.white_gray};
   margin: 15px 0 25px;
-`
+  border: 0.5px solid ${COLORS.white_gray};
+`;
 
 const InputBox = styled.div`
   margin: 50px 0 150px;
-`
+`;
 
 const Desc = styled.div`
   margin: 15px 25px;
   font-size: 15px;
 `;
 
-const Input = styled.input`
-  width: 100%;
-  margin-left: 15px;
-  padding: 0 15px;
-  height: 48px;
-  color: ${COLORS.black};
-  margin-bottom: 22px;
-  border: 1px solid ${COLORS.light_gray};
-  background: none;
-  &::placeholder {
-    font-size: 13px;
-    color: ${COLORS.light_gray};
-  }
-  &:focus {
-    outline: none;
-    border: 1px solid ${COLORS.blue};
-  }
-`
-
 const BtnBox = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 30px;
   gap: 20px;
+  margin-top: 30px;
 `;
 
 const Btn = styled.button`
-  font-size: 14px;
+  margin: 10px 0 0;
   width: 100px;
   height: 40px;  
-  margin: 10px 0 0;
-  /* border-radius: 4px; */
-  color: ${COLORS.light_gray};
   border: 1px solid ${COLORS.light_gray};
+  color: ${COLORS.light_gray};
+  font-size: 14px;
   &:hover{
     border: none;
+    background: ${COLORS.blue};
     color: ${COLORS.white};
-    background-color: ${COLORS.blue};
   }
   &:disabled {
     opacity: 0.5;
   }
-`
+`;
